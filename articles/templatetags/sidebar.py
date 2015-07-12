27ad6153889bb1a13c65034e554d8b374ed6d444
Ivan Simeyko <iv.simeyko@gmail.com>
2015-07-12 11:14:46 +0300

    добавил статью о создании тегов

---
+++ articles/templatetags/sidebar.py
@@ -0,0 +1,13 @@
+from django import template
+from articles.models import Article, Tag, Category, Comment
+
+register = template.Library()
+
+@register.inclusion_tag("right_sidebar.html")
+def show_sidebar():
+    tags = Tag.objects.all()
+    categories = Category.objects.all().order_by("name")
+    popular_articles = Article.objects.all().order_by('-article_likes')[:5]
+    last_comment = Comment.objects.all().order_by('comment_date')[:5]
+    return {'tags': tags, 'categories': categories, 'popular_articles': popular_articles, 'last_comment': last_comment}
+
