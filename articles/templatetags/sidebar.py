23812a9173051ac36844f86110c6e409a09e3bfb
Ivan Simeyko <iv.simeyko@gmail.com>
2015-08-05 06:03:11 +0300

    Исправил порядок следования тегов в сайдбаре

--- articles/templatetags/sidebar.py
+++ articles/templatetags/sidebar.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.inclusion_tag("right_sidebar.html")
 def show_sidebar():
-    tags = Tag.objects.all()
+    tags = Tag.objects.all().order_by('name')
     categories = Category.objects.all().order_by("name")
     popular_articles = Article.objects.published().order_by('-article_likes')[:5]
     last_comment = Comment.objects.all().order_by('comment_date')[:5]
