79283563b0f18308ea561d5f70dca7410df19cb0
Ivan Simeyko <iv.simeyko@gmail.com>
2016-03-25 22:37:21 +0200

    modify settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))   # this path to the upper folder the current file
 
 
 # Quick-start development settings - unsuitable for production
