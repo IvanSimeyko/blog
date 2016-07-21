0af6459048374b8d8f6acc68535d19c2998f84ea
IvanSimeyko <iv.simeyko@gmail.com>
2016-07-21 21:35:54 +0300

    fixed the order of comments in sidebar

--- articles/templatetags/sidebar.py
+++ articles/templatetags/sidebar.py
@@ -8,6 +8,6 @@ def show_sidebar():
     tags = Tag.objects.all().order_by('name')
     categories = Category.objects.all().order_by("name")
     popular_articles = Article.objects.published().order_by('-article_likes')[:5]
-    last_comment = Comment.objects.all().order_by('comment_date')[:5]
+    last_comment = Comment.objects.all().order_by('-comment_date')[:5]
     return {'tags': tags, 'categories': categories, 'popular_articles': popular_articles, 'last_comment': last_comment}
 
